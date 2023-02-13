65f7f822583ba5e9f7a6143fcdb49736501185f5
Asont <a.druzhko@gmail.com>
2023-02-13 16:01:15 +0300

    HW10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,14 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+    <svg xmlns="http://www.w3.org/2000/svg"
+         style={{margin: 'auto', background: 'none', display: 'block', shapeRendering: 'auto', width:'204px', height:'204px'}}
+         viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+        <path d="M24 50A26 26 0 0 0 76 50A26 28 0 0 1 24 50" fill="#512de4" stroke="none">
+            <animateTransform attributeName="transform" type="rotate" dur="1.1363636363636365s" repeatCount="indefinite"
+                              keyTimes="0;1" values="0 50 51;360 50 51"/>
+        </path>
+      </svg>
+
+</div>
+
